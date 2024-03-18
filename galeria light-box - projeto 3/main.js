8cfeede37cb58c7cb95f51e233e8830e9d10a909
Alexandrezapsss <alexandrequeirozdasilva94@gmail.com>
2024-03-18 08:31:53 -0300

    retoques no CSS e JS

--- galeria light-box - projeto 3/main.js
+++ galeria light-box - projeto 3/main.js
@@ -1,6 +1,25 @@
-function testeAlert(){
-    alert('Mensagem')
-    console.log('Funciona')
-}
+const galleryItems = document.querySelectorAll(".gallery-item")//prestar atenção aqui são vários items usar o SelectorAll
+const lightbox = document.querySelector(".lightbox")
+const lightboxImage = document.querySelector(".lightbox-image")
+const lightboxClose = document.querySelector(".lightbox-close")
 
-document.querySelector("#testejs").addEventListener("click", testeAlert);
+// como vamos trabalhar com uma coleção de imagens vamos usar foreach para percorrer
+galleryItems.forEach((item) =>{
+    //agora vamos adicionar um evento de click para cada uma das imagens
+    item.addEventListener("click", () => {
+        //teste de click
+        //console.log("Click, click, click")
+        //selecionar a imagem para destaque
+        const imgUrl = item.querySelector(".gallery-image").getAttribute("data-src")
+        //agora vamos colocar essa imagem em destaque na classe lightbox-image usando setAttribute
+        lightboxImage.setAttribute("src", imgUrl)//aqui é para setar a url da imagem selecionada no src que deixamos vazio
+        lightbox.style.display = "flex"
+    })
+    
+})
+
+//vamos fazer o evento de fechar a imagem em destaque
+lightboxClose.addEventListener("click", () =>{
+    //para relembrar nossa classe lightbox no css está none porem acima ao click da img colocamos o display flex aqui só vamos retirar
+    lightbox.style.display = "none"
+})
